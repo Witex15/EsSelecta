4b336f9d6e67c397aba1b25748714981dcc9bbf4
Witex15 <noe.zwissig@bluewin.ch>
2023-05-22 16:05:38 +0200

    creat models

---
+++ EsSelecta/Models/Distributor.cs
@@ -0,0 +1,10 @@
+﻿namespace EsSelecta.Models
+{
+    public class Distributor
+    {
+        private List<Item> items { get; set; }
+        private int credit { get; set; }
+        private int balance { get; set; }
+
+    }
+}
--- EsSelecta/Models/Item.cs
+++ EsSelecta/Models/Item.cs
@@ -2,5 +2,9 @@
 {
     public class Item
     {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
     }
 }
